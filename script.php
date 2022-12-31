c3909d94323f8b5a98033e4ca8de197857bf32ff
om-yad <omyad777@gmail.com>
2023-01-01 02:57:26 +0530

    done

---
+++ script.php
@@ -0,0 +1,9 @@
+<html>
+<body>    
+    <?php
+        echo "welcome " . $_POST['first_name'];
+        echo "your last name is " . $_POST['last_name'];
+        echo "your mail address is " . $_POST['email'];
+    ?>
+</body>
+</html>
